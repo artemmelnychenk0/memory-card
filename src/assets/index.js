5cb9dfa1b22850d444cf7a3c963c04f885156a41
artem <amelnychenko06@gmail.com>
2022-07-05 19:47:03 -0400

    added images to the game

---
+++ src/assets/index.js
@@ -0,0 +1,16 @@
+import peter from "./images/peter.png"
+import brian from "./images/brian.png"
+import chris from "./images/chris.png"
+import meg from "./images/meg.png"
+import stewie from "./images/stewie.png"
+import joe from "./images/joe.png"
+import cleveland from "./images/cleveland.png"
+import lois from "./images/lois.png"
+import quagmire from "./images/quagmire.png"
+import herbert from "./images/herbert.png"
+import ghostface from "./images/ghostface.png"
+import vinny from "./images/vinny.png"
+import logo from "./images/logo.png"
+
+const images = { peter, brian, chris, meg, stewie, joe, cleveland, lois, quagmire, herbert, ghostface, vinny, logo }
+export default images
